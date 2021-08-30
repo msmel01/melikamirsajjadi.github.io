74b791c960771fa4657639364035b77424e4c4dd
msmel01 <ms.elly17.01@gmail.com>
2021-08-30 13:31:43 -0400

    added global font + navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
 <template>
-
-  <router-view/>
+  <div class="container">
+    <NavBar />
+    <router-view/>
+  </div>
 </template>
+
+<script>
+  import NavBar from '@/components/NavBar.vue';
+  export default {
+    components: {
+      NavBar,
+    }
+  }
+</script>
+
+<style lang='scss'>
+  @import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap');
+  * {
+    padding: 0px;
+    margin: 0px;
+    box-sizing: border-box;
+    font-family: 'Merriweather', serif;
+
+  }
+  .container {
+    overflow-x: hidden;
+  }
+</style>
