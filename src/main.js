a561936a1732b68004da720ee4923e5e444722c2
msmel01 <ms.elly17.01@gmail.com>
2021-08-31 12:44:26 -0400

    added particle component + hero in about

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Particles from "particles.vue3";
 
 library.add(faLinkedin, faGithub, faBars)
 
 createApp(App)
 .use(router)
+.use(Particles)
 .component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="container">
+      <Hero />
+      <Particles id="tsparticles" :options="particleConfig"/>
   </div>
 </template>
+
+<script>
+  import Hero from '@/components/Hero.vue';
+  import particleJson from '/src/assets/particle.json';
+
+  export default {
+    data() {
+      return {
+        particleConfig: particleJson,
+      }
+    },
+    components: {
+      Hero,
+    }
+  }
+
+</script>
+
+<style lang="scss">
+
+  #tsparticles {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background: linear-gradient(#0f1e35,
+                              #11223c,
+                              #122643,
+                              #142a4a,
+                              #162e51,
+                              #183258,
+                              #19365f,
+                              #1b3b67,
+                              #1d3f6e);
+  // background: linear-gradient(#1d3f6e,
+  //                             #1b3b67,
+  //                             #19365f,
+  //                             #183258,
+  //                             #162e51,
+  //                             #142a4a,
+  //                             #122643,
+  //                             #11223c,
+  //                             #0f1e35);
+  z-index: 1;
+}
+</style>
