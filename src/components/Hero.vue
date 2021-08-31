4b105f560102080137d92e4ad8044119c30f4e4f
msmel01 <ms.elly17.01@gmail.com>
2021-08-31 12:42:27 -0400

    added hero section component

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="hero">
+        <div class="container">
+            <h2 class="title">Hi, I'm
+                <span>M</span>
+                <span>e</span>
+                <span>l</span>
+                <span>i</span>
+                <span>k</span>
+                <span>a</span>.
+            </h2>
+            <p class="subtitle">Creative software developer with a passion for AI, robotics, and design.</p>
+        </div>
+    </section>
+</template>
+
+<script>
+    import '@/styles/hero.scss';
+    export default {
+        name: 'Hero',
+        data() {
+            return {
+
+            }
+        },
+    }
+</script>
