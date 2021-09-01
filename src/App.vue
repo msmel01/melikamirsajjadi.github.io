8e35a3855f5915e6a339c1690f85464276211cc9
msmel01 <ms.elly17.01@gmail.com>
2021-09-01 06:44:46 -0400

    added reverse scrolling and hide scrollbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container-header">
     <NavBar />
     <router-view/>
   </div>
@@ -7,23 +7,45 @@
 
 <script>
   import NavBar from '@/components/NavBar.vue';
+ 
   export default {
     components: {
       NavBar,
+    },
+    mounted() {
+        document.body.style.overflow = 'hidden';
+        window.onload = init;
+
+        function init() {
+    
+          document.body.addEventListener('wheel', function (event) {
+            event.preventDefault();
+            window.scrollBy({ 
+                top: -event.deltaY, // could be negative value
+                left: 0, 
+                behavior: 'smooth' 
+              });
+           
+          }, { passive: false });
+
+          }
+        }
     }
-  }
+
 </script>
 
 <style lang='scss'>
-  @import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@300&display=swap');
   * {
     padding: 0px;
     margin: 0px;
     box-sizing: border-box;
-    font-family: 'Merriweather', serif;
-
   }
   .container {
     overflow-x: hidden;
   }
+  ::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+  }
+
 </style>
