365c503fd834afa0b4cd17ae469b8052b829d71c
msmel01 <ms.elly17.01@gmail.com>
2021-09-01 06:53:01 -0400

    added education component

---
+++ src/components/Education.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="education">
+        <div class="container">
+            <p class="line1">I am studying <span class="color">Computer Science</span> and pursuing a <span class="color">French</span> certificate</p>
+            <p class="line2">at the University of Waterloo, Canada</p>
+            <p class="line3">since 2019 and until <span class="color">2024</span>.</p>
+        </div>
+    </section>
+</template>
+
+
+<script>
+    import '@/styles/edu.scss';
+
+    export default {
+        name: 'Education',
+        data() {
+            return {
+
+            }
+        },
+    }
+</script>
